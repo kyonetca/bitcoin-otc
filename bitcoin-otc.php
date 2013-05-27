e111982285148fed3c56781555be53b53f2e10e1
wikitopian <matt@swarmstrategies.com>
2013-05-27 02:41:26 +0000

    init

---
+++ bitcoin-otc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* Plugin Name: Bitcoin-OTC Automation Plugin
+* Plugin URI: https://github.com/wikitopian/bitcoin-otc
+* Description: For the #bitcoin-otc channel regular
+* Version: 0.1
+* Author: wikitopian
+* Author URI: http://www.swarmstrategies.com/matt
+* License: GPLv2
+* */
+
+class Bitcoin_OTC {
+
+}
+
+$bitcoin_otc = new Bitcoin_OTC();
+
+?>
